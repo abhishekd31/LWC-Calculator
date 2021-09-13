9b3f556ed698c6e40dc40f97b553fd3c90e0bd29
Abhishek Das <abhishek.das22786@gmail.com>
2021-09-13 00:37:54 -0400

    initial commit

---
+++ force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
@@ -0,0 +1,44 @@
+import { LightningElement, track } from 'lwc';
+
+export default class SimpleCalculator extends LightningElement {
+
+    @track currentResult;
+
+    firstNumber;
+    secondNumber;
+
+    numberChangeHandler(event){
+        const inputBoxName = event.target.name;
+        if(inputBoxName === 'firstNumber'){
+            this.firstNumber = event.target.value;
+
+        }else if(inputBoxName === 'secondNumber'){
+            this.secondNumber = event.target.value;
+        }
+    }
+
+    addHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+
+        this.currentResult = 'Result of' +firstN+ '+' +secondN+ 'is' +(firstN+secondN);
+    }
+    subtractHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+
+        this.currentResult = 'Result of' +firstN+ '-' +secondN+ 'is' +(firstN-secondN);
+    }
+    multiplyHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+
+        this.currentResult = 'Result of' +firstN+ '*' +secondN+ 'is' +(firstN*secondN);
+    }   
+    divideHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+
+        this.currentResult = 'Result of' +firstN+ '/' +secondN+ 'is' +(firstN/secondN);
+    }
+}
